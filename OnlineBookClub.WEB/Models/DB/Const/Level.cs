7ea87b4268d810784729a6a2f28e366590e80be9
xHolland <81883631+xHolland41@users.noreply.github.com>
2024-02-10 17:17:21 +0300

    Created Table
    Created Level Table

--- OnlineBookClub.WEB/Models/DB/Const/Level.cs
+++ OnlineBookClub.WEB/Models/DB/Const/Level.cs
@@ -1,6 +1,11 @@
-﻿namespace OnlineBookClub.WEB.Models.DB.Const
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBookClub.WEB.Models.DB.Const
 {
     public class Level
     {
+        [Key]
+        public byte LevelId { get; set; }
+        public int ExperiencePoint { get; set; }
     }
 }
