48a97b80910db1332a621500bf86a05eb8f8d126
Kerem Aydın <suspectcrow@outlook.com.tr>
2024-02-17 00:22:27 +0300

    Fixed some DataSeeding bugs.

--- OnlineBookClub.WEB/Data/DataSeeding.cs
+++ OnlineBookClub.WEB/Data/DataSeeding.cs
@@ -13,7 +13,11 @@ namespace OnlineBookClub.WEB.Data
 
             using (var scope = app.ApplicationServices.CreateScope())
             {
+
+
                 var context = scope.ServiceProvider.GetService<OnlineBookClubContext>()!;
+
+                context.Database.Migrate();
 
 				//? ===============| ROLE ADD |===============
 				List<string> roles = new List<string> 
@@ -40,9 +44,10 @@ namespace OnlineBookClub.WEB.Data
 					}
 				}
 
-                //? ===============| END of Role Add |===============
+				//? ===============| END of Role Add |===============
 
-                var user = new AppUser
+				//? ===============| USER ADD |===============
+				var user = new AppUser
                 {
                     CREATED_DATE = DateTime.UtcNow,
                     Email = "cevrimicikitapkulubu@gmail.com",
@@ -52,8 +57,12 @@ namespace OnlineBookClub.WEB.Data
                     NormalizedUserName = "FSAHİNBAS",
                     UserName = "FSahinbas",
                     PhoneNumber = "05123456789",
-                    SchoolId = 1,
-					SecurityStamp = Guid.NewGuid().ToString("D")
+					SecurityStamp = Guid.NewGuid().ToString("D"),
+                    AccessFailedCount = 0,
+                    LockoutEnabled = false,
+                    TwoFactorEnabled = false,
+                    PhoneNumberConfirmed = false,
+                    EmailConfirmed = false
 
 				};
 
@@ -63,15 +72,46 @@ namespace OnlineBookClub.WEB.Data
                     var hashed = password.HashPassword(user, "ExamplePass01&A");
                     user.PasswordHash = hashed;
 
-                    var userStore = new UserStore<AppUser>(context);
-                    userStore.CreateAsync(user);
+                    var userStore = new UserStore<AppUser, AppRole, OnlineBookClubContext>(context);
+                    await userStore.CreateAsync(user);
                 }
 
-                UserManager<AppUser> _userManager = scope.ServiceProvider.GetService<UserManager<AppUser>>();
-                AppUser _user = await _userManager.FindByEmailAsync(user.Email);
-                await _userManager.AddToRolesAsync(_user, new List<string> { "Super_Admin", "Teacher" });
+                UserManager<AppUser> _userManager = scope.ServiceProvider.GetService<UserManager<AppUser>>(); //! Burada sorun var, User'ı alamıyor.
+				AppUser _user = await _userManager.FindByNameAsync(user.UserName);
 
-                context.SaveChanges();
+                List<string> userRoles = new List<string> { "Super_Admin", "Teacher" };
+
+                foreach (var role in userRoles)
+                {
+                    await _userManager.AddToRoleAsync(_user, role);
+                }
+
+                //? ===============| END of User Add |===============
+
+                //? ===============| City ADD |===============
+
+                foreach (var city in context.Cities.ToList())
+                {
+                    context.Cities.Remove(city);
+                }
+
+                List<string> cities = new List<string> { "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop", "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+
+                foreach (var city in cities)
+                {
+                    await context.AddAsync(city);
+                }
+
+
+                //? ===============| END of City Add |===============
+
+                //? ===============| City ADD |===============
+
+                // Departmanlar, İlçeler, Leveller, 2 Adet Event, Schools, UserAchievements
+
+                //context.Database.ExecuteSqlRawAsync("");
+
+				context.SaveChanges();
 
 
 			}
