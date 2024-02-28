d6d25a9323c343dbc4122390925f8737573f1c0d
xHolland <81883631+xHolland0@users.noreply.github.com>
2024-02-28 19:27:28 +0300

    Part-1 of the Explore section on the admin page has been added and a few bugs have been fixed.

---
+++ OnlineBookClub.WEB/wwwroot/js/chart1.js
@@ -0,0 +1,20 @@
+﻿const context2 = document.getElementById('MonthlyEventArea');
+
+new Chart(context2, {
+    type: 'line',
+    data: {
+        labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'],
+        datasets: [{
+            label: '# of Votes',
+            data: [120, 90, 194, 172, 150, 100, 75, 135, 88, 90, 62, 40,],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
---
+++ OnlineBookClub.WEB/wwwroot/js/chart2.js
@@ -0,0 +1,20 @@
+﻿const context = document.getElementById('GenderArea');
+
+new Chart(context, {
+    type: 'polarArea',
+    data: {
+        labels: ['Erkek', 'Kız'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
